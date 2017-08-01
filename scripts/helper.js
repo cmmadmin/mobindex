24de7cd4285369e909db58c502b57109fcd0459a
don3365 <donald.heath@wayland.wbu.edu>
2017-08-01 14:11:29 -0500

    refactored for improved functions

--- scripts/helper.js
+++ scripts/helper.js
@@ -35,6 +35,56 @@ upgAvgMedianDiff ( [array of sending country data], [array of selected country d
 
  */
 
+function isEmpty(val) {
+    return (val == "na" || val == "-" || val === undefined || val == null || val.length <= 0) ? true : false;
+}
+
+function notEmpty(val) {
+    return (val == "na" || val == "-" || val === undefined || val == null || val.length <= 0) ? false : true;
+}
+
+function toPct(item){
+    return isNaN(item)? item  : (item*100).toPrecision(2)+"%"
+}
+
+function upgLineMedian (r1,r2,r3,r4,r5,r6,d1,d2,d3,d4,d5,d6){
+    var rd1= (notEmpty(r1)  &&  notEmpty(d1)) ? Math.abs(r1-d1): "-";
+    var rd2= (notEmpty(r2)  &&  notEmpty(d2)) ? Math.abs(r2-d2): "-";
+    var rd3= (notEmpty(r3)  &&  notEmpty(d3)) ? Math.abs(r3-d3): "-";
+    var rd4= (notEmpty(r4)  &&  notEmpty(d4)) ? Math.abs(r4-d4): "-";
+    var rd5= (notEmpty(r5)  &&  notEmpty(d5)) ? Math.abs(r5-d5): "-";
+    var rd6= (notEmpty(r6)  &&  notEmpty(d6)) ? Math.abs(r6-d6): "-";
+    var tempArr = [];
+    if (notEmpty(rd1) ) { tempArr.push(rd1); }
+    if (notEmpty(rd2) ) { tempArr.push(rd2); }
+    if (notEmpty(rd3) ) { tempArr.push(rd3); }
+    if (notEmpty(rd4) ) { tempArr.push(rd4); }
+    if (notEmpty(rd5) ) { tempArr.push(rd5); }
+    if (notEmpty(rd6) ) { tempArr.push(rd6); }
+    return (typeof tempArr !== 'undefined' && tempArr.length > 0)?math.median(tempArr):"-";
+}
+
+function dumpNA(item){
+    if (isEmpty(item[1])) {item[1]="-"}
+    if (isEmpty(item[2])) {item[2]="-"}
+    if (isEmpty(item[3])) {item[3]="-"}
+    if (isEmpty(item[4])) {item[4]="-"}
+    if (isEmpty(item[5])) {item[5]="-"}
+    if (isEmpty(item[6])) {item[6]="-"}
+    if (isEmpty(item[7])) {item[7]="-"}
+    if (isEmpty(item[8])) {item[8]="-"}
+    if (isEmpty(item[9])) {item[9]="-"}
+    if (isEmpty(item[10])){item[10]="-"}
+    if (isEmpty(item[11])){item[11]="-"}
+    if (isEmpty(item[12])){item[12]="-"}
+    if (isEmpty(item[13])){item[13]="-"}
+    if (isEmpty(item[14])){item[14]="-"}
+    if (isEmpty(item[15])){item[15]="-"}
+    if (isEmpty(item[16])){item[16]="-"}
+    if (isEmpty(item[17])){item[17]="-"}
+    if (isEmpty(item[18])){item[18]="-"}
+}
+
 function upgYes(dataSet) {
     // assumes full dataSet only
     // returns minimal set of upg data used in upgAvgMedian calculation
@@ -57,12 +107,12 @@ function toMinimal(arr) {
         var subarray = [];				//create blank array for storing subarray values
         //note that only values 0-6 are used
         //assumes structured data is in locations 0-6, other locations ignored
-        if (item[1] != null) { subarray.push(item[1]); }	// Assumes item[1] is Power Distance
-        if (item[2] != null) { subarray.push(item[2]); }	// Assumes item[2] is Individualism
-        if (item[3] != null) { subarray.push(item[3]); }	// Assumes item[3] is Masculinity
-        if (item[4] != null) { subarray.push(item[4]); }	// Assumes item[4] is Uncertainty Avoidance
-        if (item[5] != null) { subarray.push(item[5]); }	// Assumes item[5] is Long Term Orientation
-        if (item[6] != null) { subarray.push(item[6]); }	// Assumes item[6] is Indulgence
+        if (notEmpty(item[1]) ) { subarray.push(item[1]); }	// Assumes item[1] is Power Distance
+        if (notEmpty(item[2]) ) { subarray.push(item[2]); }	// Assumes item[2] is Individualism
+        if (notEmpty(item[3]) ) { subarray.push(item[3]); }	// Assumes item[3] is Masculinity
+        if (notEmpty(item[4]) ) { subarray.push(item[4]); }	// Assumes item[4] is Uncertainty Avoidance
+        if (notEmpty(item[5]) ) { subarray.push(item[5]); }	// Assumes item[5] is Long Term Orientation
+        if (notEmpty(item[6]) ) { subarray.push(item[6]); }	// Assumes item[6] is Indulgence
         arrTemp.push(subarray);				//push subarray values or empty array to the difference array
     });
     return arrTemp;
@@ -84,25 +134,33 @@ function upgAvgMedianDiff(mod, arr) {
         var subarray = [];				//create blank array for storing subarray values
         //note that only values 0-5 are used
         //assumes structured data is in locations 0-5 (equivalent toMinimal)
-        if (item[0] != null) { subarray.push(Math.abs(item[0] - mod[0]));  }
-        if (item[1] != null) { subarray.push(Math.abs(item[1] - mod[1]));  }
-        if (item[2] != null) { subarray.push(Math.abs(item[2] - mod[2]));  }
-        if (item[3] != null) { subarray.push(Math.abs(item[3] - mod[3]));  }
-        if (item[4] != null) { subarray.push(Math.abs(item[4] - mod[4]));  }
-        if (item[5] != null) { subarray.push(Math.abs(item[5] - mod[5]));  }
+        if (    notEmpty(item[0])  &&  notEmpty(mod[0])    ) { subarray.push(Math.abs(item[0] - mod[0]));  }
+        if (    notEmpty(item[1])  &&  notEmpty(mod[1])    ) { subarray.push(Math.abs(item[1] - mod[1]));  }
+        if (    notEmpty(item[2])  &&  notEmpty(mod[2])    ) { subarray.push(Math.abs(item[2] - mod[2]));  }
+        if (    notEmpty(item[3])  &&  notEmpty(mod[3])    ) { subarray.push(Math.abs(item[3] - mod[3]));  }
+        if (    notEmpty(item[4])  &&  notEmpty(mod[4])    ) { subarray.push(Math.abs(item[4] - mod[4]));  }
+        if (    notEmpty(item[5])  &&  notEmpty(mod[5])    ) { subarray.push(Math.abs(item[5] - mod[5]));  }
         arrDiff.push(subarray);				//push subarray values or empty array to the difference array
     });
+    if (typeof arrDiff !== 'undefined' && arrDiff.length > 0) {
+        // the array is defined and has at least one element
 
-    var arrMedian = [];					//create blank array for storing median of each subarray
-    arrDiff.forEach(function (item) {
-        if (item != null) {				//ignore null values
-            var m = math.median(item);
+        var arrMedian = [];					//create blank array for storing median of each subarray
+        arrDiff.forEach(function (item) {
+            if (typeof item !== 'undefined' && item.length > 0) {				//ignore null values
+                var m = math.median(item);
 
-            arrMedian.push(m);				//push subarray median value
-        }
-    });
-    //get average of all median values
-    var average = (arrMedian.reduce(( p, c ) => p + c, 0)/arrMedian.length);
-    return average.toPrecision(4);
+                arrMedian.push(m);				//push subarray median value
+            }
+        });
+
+        //get average of all median values
+            var average = (arrMedian.reduce(( p,c ) => p+c, 0) / arrMedian.length);
+            return isNaN(average)? "-"  : average.toPrecision(4)
+
+    }
+
+    // arrDiff was undefined
+    return "-";
 
 }
